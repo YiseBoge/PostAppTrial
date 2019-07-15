06121e282c2c7b345c938b73f28629adc6b96e93
yiseboge <Ethiopia1!>
2019-07-15 14:27:43 +0300

    Initial Commit

---
+++ PostApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PostApp
+//
+//  Created by Yisehak Bogale on 7/15/19.
+//  Copyright © 2019 Yisehak Bogale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
